652db9b688bd677731932770f3a60c212976604b
arkiver <arkiver@protonmail.com>
2023-02-02 14:01:40 +0100

    Version 20230202.01. Fix check for if a pastebin past exists.

--- pastebin.lua
+++ pastebin.lua
@@ -140,7 +140,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
     and not string.match(url, "^https?://[^/]+/embed_iframe/") then
     html = read_file(file)
     if string.match(url, "^https?://pastebin%.com/[0-9a-zA-Z]+$")
-      and not string.match(html, 'class="textarea"') then
+      and not string.match(html, 'class="highlighted%-code"') then
       if string.match(html, "<title>%s*Private Paste ID") then
         print("Private paste.")
       elseif string.match(html, "<title>[^<]+Potentially offensive content") then
